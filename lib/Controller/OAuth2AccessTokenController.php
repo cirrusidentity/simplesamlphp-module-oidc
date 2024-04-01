92baf0aa7b0974627444b46403bb2c42500adcaa
Patrick Radtke <patrick@cirrusidentity.com>
2024-04-01 14:23:37 -0700

    Allow options requests to token endpoint

--- lib/Controller/OAuth2AccessTokenController.php
+++ lib/Controller/OAuth2AccessTokenController.php
@@ -14,9 +14,11 @@
 
 namespace SimpleSAML\Module\oidc\Controller;
 
+use SimpleSAML\Module\oidc\Repositories\AllowedOriginRepository;
 use SimpleSAML\Module\oidc\Server\AuthorizationServer;
 use Laminas\Diactoros\Response;
 use Laminas\Diactoros\ServerRequest;
+use SimpleSAML\Module\oidc\Server\Exceptions\OidcServerException;
 
 class OAuth2AccessTokenController
 {
@@ -25,13 +27,50 @@ class OAuth2AccessTokenController
      */
     private $authorizationServer;
 
-    public function __construct(AuthorizationServer $authorizationServer)
+    private AllowedOriginRepository $allowedOriginRepository;
+
+    public function __construct(AuthorizationServer $authorizationServer,  AllowedOriginRepository $allowedOriginRepository)
     {
         $this->authorizationServer = $authorizationServer;
+        $this->allowedOriginRepository = $allowedOriginRepository;
+
     }
 
     public function __invoke(ServerRequest $request): \Psr\Http\Message\ResponseInterface
     {
+        // Check if this is actually a CORS preflight request...
+        if (strtoupper($request->getMethod()) === 'OPTIONS') {
+            return $this->handleCors($request);
+        }
         return $this->authorizationServer->respondToAccessTokenRequest($request, new Response());
+    }
+
+    /**
+     * Handle CORS 'preflight' requests by checking if 'origin' is registered as allowed to make HTTP CORS requests,
+     * typically initiated in browser by JavaScript clients.
+     * @param ServerRequest $request
+     * @return Response
+     * @throws OidcServerException
+     */
+    protected function handleCors(ServerRequest $request): Response
+    {
+        $origin = $request->getHeaderLine('Origin');
+
+        if (empty($origin)) {
+            throw OidcServerException::requestNotSupported('CORS error: no Origin header present');
+        }
+
+        if (! $this->allowedOriginRepository->has($origin)) {
+            throw OidcServerException::accessDenied(sprintf('CORS error: origin %s is not allowed', $origin));
+        }
+
+        $headers = [
+            'Access-Control-Allow-Origin' => $origin,
+            'Access-Control-Allow-Methods' => 'GET, POST, OPTIONS',
+            'Access-Control-Allow-Headers' => 'Authorization',
+            'Access-Control-Allow-Credentials' => 'true',
+        ];
+
+        return new Response('php://memory', 204, $headers);
     }
 }
